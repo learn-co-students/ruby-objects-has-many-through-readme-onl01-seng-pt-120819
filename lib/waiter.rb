2695ac3a76c345324d99b028aab40ec2f5b96bbb
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-16 18:36:26 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
+ attr_accessor :name, :yrs_experience
+  @@all = []
+  
+  def initialize (name, yrs_experience)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
