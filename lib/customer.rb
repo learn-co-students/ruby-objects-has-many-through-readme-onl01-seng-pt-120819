1d9eaa3b992ff8726e9c84ad4bd40d59829b512f
Damely Tineo <damely.tineo07@gmail.com>
2020-01-23 19:20:07 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,54 @@
+#has many waiters through meals
 class Customer
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
   
+  def new_meal(waiter, total, tip=0) #grants the/a customer the ability to create a new meal. it automatically associates each new meal with the customer that created it. 
+    Meal.new(waiter, self, total, tip) #same
+  end
+  
+  def meals #the customer is going to look at all of the meals, and then select only the ones that belong to them, if they know the name of the waiter of their last meal so they can leave them a good review. 
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+  
+  def waiters #So rachel.meals will return an array of all of Rachel's meals, but what if we now want a list of all of the waiters that Rachel has interacted with? Each meal is also associated with a waiter, so to get every waiter from every meal Rachel has had, we need to take the array of all of Rachel's meals, map over it, getting the waiter from each of those meals.
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+  
+  def new_meal(waiter, total, tip=0) #?
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+  def new_meal_20_percent(waiter, total) #?
+    tip = total * 0.2
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+  def self.oldest_customer #?
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+   
+  def new_meal(customer, total, tip=0)#grants the/a waiter the ability to create a new meal. it automatically associates each new meal with the waiter that created it. 
+    Meal.new(self, customer, total, tip) #same
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self #waiter checks for all of the meals they have ever served
+    end
+  end
+  
+  def best_tipper #finds their best tipper 
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+ 
+    best_tipped_meal.customer
+  end
+  
+  
 end
